06046b94505b0f40f8078185ab317313358cee2f
mech-master <2906465@gmail.com>
2025-12-08 16:17:14 +0300

    Fixid server port

--- src/server/server-app.js
+++ src/server/server-app.js
@@ -17,7 +17,7 @@ import App from "../client/App";
 
 
 const isProduction = process.env.NODE_ENV === "production";
-const PORT = process.env.PROT || 3000;
+const PORT = process.env.PORT || 3000;
 
 
 //Создание сервера
